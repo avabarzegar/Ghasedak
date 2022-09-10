ed95fbafe145b4d9ee4ea025231b8acdc3a951f9
fatemeh barzegar <barzegarfa81@gmail.com>
2022-09-10 13:04:56 -0400

    adding general styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StyledButton from './components/UI/Button/Button';
+
+const App =()=>{
+    return(
+        <div>
+           
+        </div>
+    )
+}
+
+export default App;

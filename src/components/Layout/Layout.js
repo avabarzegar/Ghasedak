ff3213980e536b21ebca027a7729b906d6d4203e
ali rezaee <ali.re799@gmail.com>
2022-09-11 13:41:38 +0430

    hero slider without responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,23 @@ import React from 'react'
 import Navbar from '../Navigation/Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import './Layout.css'
+import Hreoslider from '../Heroslider/Hreoslider';
+
+
 
 const Layout = (props) => {
   return (
     <div>
+      <div>
       <Navbar />
+      
       <main className="content">{props.children}</main>
       <Footer />
+      
+    </div>
+    <div>
+      <Hreoslider />
+    </div>
     </div>
   )
 }

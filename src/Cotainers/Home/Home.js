cca9db8bc50598e656055ad03ddef89f1250c800
ali rezaee <ali.re799@gmail.com>
2022-09-15 21:25:27 +0430

    not responsive

--- src/Cotainers/Home/Home.js
+++ src/Cotainers/Home/Home.js
@@ -4,6 +4,7 @@ import BookSliderFirst from "../../components/BookSliderFirst/BookSliderFirst";
 import { Container } from "react-bootstrap";
 import Nearestshops from '../../components/nearestshops/nearestshops'
 import Bestshops from '../../components/bestshops/Bestshops'
+import Offer from "../../components/offer/Offer";
 const Home =()=>{
     return(
     // === the first type of slider used in home page ===
@@ -12,6 +13,7 @@ const Home =()=>{
         <BookSliderFirst />
         <Nearestshops />
         <Bestshops />
+        <Offer />
     </Container>
     // === the first type of slider used in home page end ===
 
--- src/components/offer/pricediscount/PriceandDiscount.js
+++ src/components/offer/pricediscount/PriceandDiscount.js
@@ -1,12 +1,20 @@
 import React from "react";
 import './PriceandDiscount.css';
 import Toman from '../../../assets/Images/icon/toman.svg'
-
+import PriceLabel from "./PriceLabel";
 
 const PriceandDiscount = () => {
    return (
       <div className="parnet-section-price">
-
+         <div className="green-price-parent">
+            <PriceLabel className='green-price-child'>50.000</PriceLabel>
+            <img src={Toman} alt='price img' />
+         </div>
+         <div className="red-price-child">
+            <PriceLabel>100.000</PriceLabel>
+         </div>
       </div>
    )
 }
+
+export default PriceandDiscount;

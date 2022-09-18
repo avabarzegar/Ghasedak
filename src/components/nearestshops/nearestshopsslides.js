2777e40db4dc408b02ef9a47440cdabdc0dd91fd
ali rezaee <ali.re799@gmail.com>
2022-09-18 11:08:14 +0430

    add nears

--- src/components/nearestshops/nearestshopsslides.js
+++ src/components/nearestshops/nearestshopsslides.js
@@ -1,7 +1,7 @@
 // Import Swiper React components
-import {  SwiperSlide ,Swiper } from 'swiper/react';
-import SwiperCore,{ Virtual , Navigation , Autoplay } from 'swiper';
-import { React ,useState ,useEffect , useRef ,useCallback}  from 'react';
+import { SwiperSlide, Swiper } from 'swiper/react';
+import SwiperCore, { Virtual, Navigation, Autoplay } from 'swiper';
+import { React, useState, useEffect, useRef, useCallback } from 'react';
 import axios from 'axios';
 import SliderTopLine from '../SliderTopLine/SliderTopLine';
 import SliderNavigation from '../SliderNavigation/SliderNavigation';
@@ -13,104 +13,104 @@ import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 
 import './nearestshopsslides.css';
+import { Container } from 'react-bootstrap';
 
 // === mapping swiper slides ===
-const Nearestshopsslides =()=>{
-    
+const Nearestshopsslides = () => {
+
   // refer hook
-  const navigationPrevRef = useRef(null) 
+  const navigationPrevRef = useRef(null)
   const navigationNextRef = useRef(null)
 
   // refer hook end
-  
+
   // state hook 
   const [swiperRef, setSwiperRef] = useState();
-    const [slide , setSlide] =useState([]);
-    const [swiperChange , setSwiperChange] =useState();
+  const [slide, setSlide] = useState([]);
+  const [swiperChange, setSwiperChange] = useState();
   // state hook end
 
-    useEffect(()=>{
-       
-    //    === get slides data from api ===
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then((response)=>{
-            const slideData=[];
-            for(const item in response.data.splice(0,12)){
-                slideData.push({
-                    id:item ,
-                    title:response.data[item].title,
-                    body:response.data[item].body
-                })     
-            }
-           setSlide(slideData)
-        })
-        .catch((err)=>{
-            console.log(err.message)
-        })
+  useEffect(() => {
 
-      
-     
+    //    === get slides data from api ===
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        const slideData = [];
+        for (const item in response.data.splice(0, 12)) {
+          slideData.push({
+            id: item,
+            title: response.data[item].title,
+            body: response.data[item].body
+          })
+        }
+        setSlide(slideData)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+
+
+
     //    === get slides data from api ===
 
-    } , [])
-    
-    // == use swiper autoplay ==
-    // SwiperCore.use([Autoplay])
-    // == use swiper autoplay end ==
-    SwiperCore.use([Navigation])
-   
-    const handleLeftClick = useCallback(() => {
-      if (!swiperRef) return;
-      swiperRef.slidePrev();
-      swiperChange.slideTo(0)
-    }, [swiperRef]);
- 
-    const handleRightClick = useCallback(() => {
-      if (!swiperRef) return;
-      swiperRef.slideNext();
-      swiperChange.slideTo(0)
-    }, [swiperRef]);
- 
+  }, [])
+
+  // == use swiper autoplay ==
+  // SwiperCore.use([Autoplay])
+  // == use swiper autoplay end ==
+  SwiperCore.use([Navigation])
+
+  const handleLeftClick = useCallback(() => {
+    if (!swiperRef) return;
+    swiperRef.slidePrev();
+    swiperChange.slideTo(0)
+  }, [swiperRef]);
+
+  const handleRightClick = useCallback(() => {
+    if (!swiperRef) return;
+    swiperRef.slideNext();
+    swiperChange.slideTo(0)
+  }, [swiperRef]);
+
   //  let SwiperSlide;
 
-    return(
-       <>
-        {/* first section of slider  */}
-        <SliderTopLine 
-         text='نزدیکترین فروشگاه ها  '
-         textBorder='purple-bottom'
-         paddingText='padding-bottom'
-         >
-         <div className='first-slider-left-container'>
-            
-        {/* navigation  */}
+  return (
+    <>
+      {/* first section of slider  */}
+      <SliderTopLine
+        text='نزدیکترین فروشگاه ها  '
+        textBorder='purple-bottom'
+        paddingText='padding-bottom'
+      >
+        <div className='first-slider-left-container'>
+
+          {/* navigation  */}
           <SliderNavigation
-          handleLeftClick={handleLeftClick}
-          handleRightClick={handleRightClick}
-          navigationPrevRef={navigationPrevRef}
-          navigationNextRef={navigationNextRef}
-          />   
-        {/* navigation end */}
-         </div>
-         </SliderTopLine>
-        {/* first section of slider end  */}
+            handleLeftClick={handleLeftClick}
+            handleRightClick={handleRightClick}
+            navigationPrevRef={navigationPrevRef}
+            navigationNextRef={navigationNextRef}
+          />
+          {/* navigation end */}
+        </div>
+      </SliderTopLine>
+      {/* first section of slider end  */}
 
-
-
+      <Container className='parent-seeall-slider-near'>
         {/* // slider  */}
         <Swiper
           className='nearshop-swiper'
           onSlideChange={setSwiperChange}
           onSwiper={setSwiperRef}
-          modules={[Navigation , Virtual]}
+          modules={[Navigation, Virtual]}
           dir={'rtl'}
-          autoplay={{delay: 2000}}
-          spaceBetween={20} 
-          slidesPerView ={'auto'}
+          autoplay={{ delay: 2000 }}
+          spaceBetween={20}
+          slidesPerView={'auto'}
           initialSlide={0}
           slidesPerGroup={1}
           loop={false}
-          slideToClickedSlide={(0,false,false)}
+          slideToClickedSlide={(0, false, false)}
           navigation={{
             prevEl: navigationPrevRef.current,
             nextEl: navigationNextRef.current,
@@ -126,27 +126,24 @@ const Nearestshopsslides =()=>{
 
         >
 
-          {/* seeallshops btn */}
-          <SeeAllShops />
-       
-         {/* seeallshops btn */}
+
 
           {slide.map((item) => {
-            return(
-                // == return swiper slides ==
+            return (
+              // == return swiper slides ==
 
-                <SwiperSlide className='nearshop-slide width-slides' key={item.id}>
-                  {()=>{
-                    if(item.id==0){
-                      return <Singleslidenearestshops />
-                    }
-                    else{
-                      return <Singleslidenearestshops />
-                    }
+              <SwiperSlide className='nearshop-slide width-slides' key={item.id}>
+                {() => {
+                  if (item.id == 0) {
+                    return <Singleslidenearestshops />
                   }
+                  else {
+                    return <Singleslidenearestshops />
+                  }
+                }
 
-                  }
-                    {/* {({ isActive }) => {
+                }
+                {/* {({ isActive }) => {
                         if(isActive){
                             return <ActiveSlide />
                         }
@@ -155,14 +152,23 @@ const Nearestshopsslides =()=>{
                         }
                       }
                     } */}
-                </SwiperSlide>
-                // == return swiper slides end ==
-            )           
+              </SwiperSlide>
+              // == return swiper slides end ==
+            )
           })}
-        </Swiper>  
+        </Swiper>
         {/* // slider end */}
+         {/* seeallshops btn */}
+         <SeeAllShops />
+       
+       {/* seeallshops btn */}
+      </Container>
+
+
+
+
     </>
-    )
+  )
 }
 
 

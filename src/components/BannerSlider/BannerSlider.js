a0af7b0c1d54197b725ac1b4c06e4f03022e64b6
ali rezaee <ali.re799@gmail.com>
2022-09-19 19:56:28 +0430

    api slider hero

---
+++ src/components/BannerSlider/BannerSlider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const BannerSlider = (props) => {
+   return (
+      <img className={`${'bannerstyle'} BannerSlider`} src={props.image} alt={props.alt} />
+   )
+}
+
+export default BannerSlider;
--- src/components/Heroslider/Hreoslider.js
+++ src/components/Heroslider/Hreoslider.js
@@ -1,66 +1,67 @@
+import { useEffect, React, useState } from "react";
 import "../Heroslider/Heroslider.css";
 import Sliderpic from '../../assets/Images/Rectangle 2.png'
 import Carousel from 'react-bootstrap/Carousel';
 import { Container } from 'react-bootstrap';
+import axios from "axios";
+import BannerSlider from "../BannerSlider/BannerSlider";
 
-const Hreoslider = () => {
+
+const Hreoslider = (props) => {
+
+   const [banner, setBanner] = useState([]);
+
+   useEffect(() => {
+      const token = 'P8o22ywHiArtk7055H4e6b5gLQbGc7gtuWJhKkJJ';
+
+      const config = {
+         headers: { Authorization: `Bearer ${token}` }
+      };
+
+      const bodyParameters = {
+         key: "value"
+      };
+
+
+      //    === get slides data from api ===
+      axios.post(
+         'application/home',
+         bodyParameters,
+         config
+      ).then((response) => {
+        
+         const bannerData = [];
+         response.data.banners.data[0].items.data.map((item) => {
+            bannerData.push({
+               id: item.id,
+               title: item.title,
+               image: item.image_url,               
+            })
+            
+         })
+         
+         setBanner(bannerData)
+      })
+         .catch((err) => {
+            console.log(err.message)
+         })
+
+
+
+      //    === get slides data from api ===
+
+   }, [])
    return (
       <Container className="container_section_heroslider">
          <Carousel className='slider_parent_section'>
-            <Carousel.Item interval={1000}>
-               <a href='#'>
-                  <img
-                     className="d-block w-100 image_slider"
-                     src={Sliderpic}
-                     alt="First slide"
-                  />
-               </a>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-               <a href='#'>
-                  <img
-                     className="d-block w-100 image_slider"
-                     src={Sliderpic}
-                     alt="Second slide"
-                  />
-               </a>
-            </Carousel.Item>
-            <Carousel.Item>
-               <a href='#'>
-                  <img
-                     className="d-block w-100 image_slider"
-                     src={Sliderpic}
-                     alt="Third slide"
-                  />
-               </a>
-            </Carousel.Item>
-            <Carousel.Item>
-               <a href='#'>
-                  <img
-                     className="d-block w-100 image_slider"
-                     src={Sliderpic}
-                     alt="Third slide"
-                  />
-               </a>
-            </Carousel.Item>
-            <Carousel.Item>
-               <a href='#'>
-                  <img
-                     className="d-block w-100 image_slider"
-                     src={Sliderpic}
-                     alt="Third slide"
-                  />
-               </a>
-            </Carousel.Item>
-            <Carousel.Item>
-               <a href='#'>
-                  <img
-                     className="d-block w-100 image_slider"
-                     src={Sliderpic}
-                     alt="Third slide"
-                  />
-               </a>
-            </Carousel.Item>
+            {banner.map((item) => {
+               return (
+                  <Carousel.Item interval={1200} key={item.id}>
+                     <BannerSlider image={item.image} />
+                     {/* <img src={item.image} /> */}
+                  </Carousel.Item>
+               )
+            })}
          </Carousel>
       </Container>
    );

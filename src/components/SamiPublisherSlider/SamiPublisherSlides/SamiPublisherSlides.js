5acbe30685ec295d217a7be60f7f254fa2c9450f
ali rezaee <ali.re799@gmail.com>
2022-10-29 13:33:01 +0330

    done

--- src/components/SamiPublisherSlider/SamiPublisherSlides/SamiPublisherSlides.js
+++ src/components/SamiPublisherSlider/SamiPublisherSlides/SamiPublisherSlides.js
@@ -1,15 +1,16 @@
 // Import Swiper React components
-import {  SwiperSlide ,Swiper } from 'swiper/react';
-import SwiperCore,{  Navigation , Autoplay } from 'swiper';
-import { React , useState ,useEffect , useRef , useCallback}  from 'react';
+import { SwiperSlide, Swiper } from 'swiper/react';
+import SwiperCore, { Navigation, Autoplay } from 'swiper';
+import { React, useState, useEffect, useRef, useCallback } from 'react';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 import SliderTopLine from '../../SliderTopLine/SliderTopLine';
 import SliderNavigation from '../../SliderNavigation/SliderNavigation';
 import BookSimpleCard from '../../BookCard/BookSimpleCard/BookSimpleCard';
 import SeeAllShops from '../../seeallshops/Seeallshops';
+import { useProductsContext } from "../../../Context/ProductContext/ProductContext";
 import LinkedCard from '../../LinkedCard/LinkedCard';
-import './SamiPublisherSlides.css';
+
 
 // Import Swiper styles
 import 'swiper/css';
@@ -17,176 +18,174 @@ import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import SliderLayout from '../../SliderLayout/SliderLayout';
 
-import { useProductsContext } from "../../../Context/ProductContext/ProductContext";
 
 
 
 // === mapping swiper slides ===
-const PopularBooksSlides =(props)=>{
-    
+const MostSaleYearSlides = () => {
+
   // refer hook
   const navigationPrevRef = useRef(null)
   const navigationNextRef = useRef(null)
   // refer hook end
-  
+
   // state hook 
   const [swiperRef, setSwiperRef] = useState();
-  const [slide , setSlide] =useState([]);
+  const [slide, setSlide] = useState([]);
   const { setBookName, bookName } = useProductsContext();
   const [title, setTitle] = useState("");
 
   // state hook end
 
 
-  useEffect(()=>{
-    const token ='DzTwF4yts6KjdR8NLdQdUtN0Y4YbcT35pVTy1Kek';
-   
+  useEffect(() => {
+    const token = 'DzTwF4yts6KjdR8NLdQdUtN0Y4YbcT35pVTy1Kek';
+
     const config = {
       headers: { Authorization: `Bearer ${token}` }
-  };
-  
-  const bodyParameters = {
-     key: "value"
-  };
-  
-   
+    };
+
+    const bodyParameters = {
+      key: "value"
+    };
+
+
     //    === get slides data from api ===
-    axios.post( 
+    axios.post(
       '/user/pwa/home',
       bodyParameters,
       config
-    ).then((response)=>{
-            const slideData=[];
-            response.data.book_lists[5].books.data.map((item)=>{
-              slideData.push({
-                  id:item.id ,
-                  name:item.name,
-                  image:item.images.data[0].image_url,
-                  price:item.best_price,
-                });
-                 
-            });
+    ).then((response) => {
+      const slideData = [];
+      response.data.book_lists[5].books.data.map((item) => {
+        slideData.push({
+          id: item.id,
+          name: item.name,
+          image: item.images.data[0].image_url,
+          price: item.best_price,
+        });
 
-            setTitle(response.data.book_lists[5].title);
-
-            setSlide(slideData)
-        })
-        .catch((err)=>{
-            console.log(err.message)
-        })
-
+      });
       
-     
+      setTitle(response.data.book_lists[5].title);
+
+      setSlide(slideData);
+    })
+      .catch((err) => {
+        console.log(err.message)
+      })
+
+
+
     //    === get slides data from api ===
 
-    } , [])
-    
-    // == use swiper core autoplay / navigation ==
-    SwiperCore.use([Autoplay , Navigation])
-    // == use swiper autoplay core autoplay / navigation end ==
-    
-   
-    const handleLeftClick = useCallback(() => {
-      if (!swiperRef) return;
-      swiperRef.slidePrev();
-    }, [swiperRef]);
- 
-    const handleRightClick = useCallback(() => {
-      if (!swiperRef) return;
-      swiperRef.slideNext();
-    }, [swiperRef]);
- 
+  }, [])
+
+  // == use swiper core autoplay / navigation ==
+  SwiperCore.use([Autoplay, Navigation])
+  // == use swiper autoplay core autoplay / navigation end ==
+
+
+  const handleLeftClick = useCallback(() => {
+    if (!swiperRef) return;
+    swiperRef.slidePrev();
+  }, [swiperRef]);
+
+  const handleRightClick = useCallback(() => {
+    if (!swiperRef) return;
+    swiperRef.slideNext();
+  }, [swiperRef]);
+
   //  let SwiperSlide;
 
-    return(
+  return (
 
-      <section className='popular-books-first popular-books'>
-        {/* first section of slider  */}
+    <section>
+      {/* first section of slider  */}
 
-        <SliderTopLine 
-         text={title}
-         textBorder='purple-bottom'
-         containerBorder='gray-bottom'
-         >
-         <div className='first-slider-left-container'>
-        {/* navigation  */}
+      <SliderTopLine
+        text={title}
+        textBorder='purple-bottom'
+        containerBorder='gray-bottom'
+      >
+        <div className='first-slider-left-container'>
+          {/* navigation  */}
           <SliderNavigation
-          handleLeftClick={handleLeftClick}
-          handleRightClick={handleRightClick}
-          navigationPrevRef={navigationPrevRef}
-          navigationNextRef={navigationNextRef}
-          />   
-        {/* navigation end */}
-         </div>
-         </SliderTopLine>
+            handleLeftClick={handleLeftClick}
+            handleRightClick={handleRightClick}
+            navigationPrevRef={navigationPrevRef}
+            navigationNextRef={navigationNextRef}
+          />
+          {/* navigation end */}
+        </div>
+      </SliderTopLine>
 
-        {/* first section of slider end  */}
+      {/* first section of slider end  */}
 
 
-        {/* slider container section  */}
-        <SliderLayout>
+      {/* slider container section  */}
+      <SliderLayout>
 
-          {/* // slider  */}
-          <Col xl={11} xs={10} className='type-two-book-slider-container'>
-        <Swiper
-          className='type-two-book-slider'
-          modules={[Navigation , Autoplay ]}
-          onSwiper={setSwiperRef}
-          autoplay={{delay: 2000}}
-          
-          slidesPerGroup={1}
-          navigation={{
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            swiper.params.navigation.prevEl = navigationPrevRef.current;
-            swiper.params.navigation.nextEl = navigationNextRef.current;
-          }}
-          scrollbar={{
-            el: '.swiper-scrollbar',
-            draggable: true,
-          }}
+        {/* // slider  */}
+        <Col xl={11} xs={10} className='type-two-book-slider-container'>
+          <Swiper
+            className='type-two-book-slider'
+            modules={[Navigation, Autoplay]}
+            onSwiper={setSwiperRef}
+            autoplay={{ delay: 2000 }}
 
-          breakpoints= {{
-           
-           
+            slidesPerGroup={1}
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current,
+            }}
+            onBeforeInit={(swiper) => {
+              swiper.params.navigation.prevEl = navigationPrevRef.current;
+              swiper.params.navigation.nextEl = navigationNextRef.current;
+            }}
+            scrollbar={{
+              el: '.swiper-scrollbar',
+              draggable: true,
+            }}
 
-            0:{
-              slidesPerView: 1 ,
-              spaceBetween: 0
-            },
-            576: {
-              slidesPerView: 1.5 ,
-              spaceBetween: 25
-            },
+            breakpoints={{
 
-            
-            768: {
-              slidesPerView: 1.5 ,  
-              spaceBetween: 25
-            },
 
-           
-            992:{
-              slidesPerView: 3 ,
-            },
 
-            
-            1200 :{
-              slidesPerView: 3.5 ,
-            },
+              0: {
+                slidesPerView: 1,
+                spaceBetween: 0
+              },
+              576: {
+                slidesPerView: 1.5,
+                spaceBetween: 25
+              },
 
-            
-            1400 : {
-              slidesPerView: 4 ,
-            }
-          }}
-        >
-        
 
-        {slide.map((item) => {
-            return(
+              768: {
+                slidesPerView: 1.5,
+                spaceBetween: 25
+              },
+
+
+              992: {
+                slidesPerView: 3,
+              },
+
+
+              1200: {
+                slidesPerView: 3.5,
+              },
+
+
+              1400: {
+                slidesPerView: 4,
+              }
+            }}
+          >
+
+            {slide.map((item) => {
+              return (
                 // == return swiper slides ==
 
                 <SwiperSlide className="book-slide-two" key={item.id}>
@@ -199,29 +198,29 @@ const PopularBooksSlides =(props)=>{
                   />
                 </SwiperSlide>
                 // == return swiper slides end ==
-            )           
-          })}
-          
-        </Swiper>  
-          </Col>
+              )
+            })}
+
+          </Swiper>
+        </Col>
         {/* // slider end */}
 
-         <Col xl={1} xs={2} className='see-all-column'>
-         {/* see all products  */}
-           <SeeAllShops
-           height='height-book-slider'
-           title='مشـــــاهده  همـــــــــــــه  محصــولات '
-           />
+        <Col xl={1} xs={2} className='see-all-column'>
+          {/* see all products  */}
+          <SeeAllShops
+            height='height-book-slider'
+            title='مشـــــاهده  همـــــــــــــه  محصــولات '
+          />
           {/* see all products end */}
-         </Col>
+        </Col>
 
 
-        </SliderLayout>
-        {/* slider container section end */}
+      </SliderLayout>
+      {/* slider container section end */}
 
-      </section>
-    )
+    </section>
+  )
 }
 
 
-export default PopularBooksSlides;
+export default MostSaleYearSlides;

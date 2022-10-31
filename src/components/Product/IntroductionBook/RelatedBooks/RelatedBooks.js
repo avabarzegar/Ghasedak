d092c797319a1bdea3ef9396c4a63aeffc3da5ab
ali rezaee <ali.re799@gmail.com>
2022-10-31 20:44:02 +0330

    sdf

--- src/components/Product/IntroductionBook/RelatedBooks/RelatedBooks.js
+++ src/components/Product/IntroductionBook/RelatedBooks/RelatedBooks.js
@@ -22,43 +22,6 @@ const ThirdPublisherSlider = (props) => {
    const { setBookId, BookId } = useProductsContext();
    const { suggestSlider } = useHomeContext();
 
-   // useEffect(() => {
-   //    const token = "qtjAvo6VkoiFRlQ7lufYbRh3R4u6vEnKEN19JKSz";
-
-   //    const config = {
-   //       headers: { Authorization: `Bearer ${token}` },
-   //    };
-
-   //    const bodyParameters = {
-   //       key: "value",
-   //    };
-
-   //    //    === get slides data from api ===
-   //    axios
-   //       .post("/user/pwa/home", bodyParameters, config)
-   //       .then((response) => {
-   //          const slideData = [];
-   //          response.data.suggestions.data.slice(0, 5).map((item) => {
-   //             slideData.push({
-   //                id: item.id,
-   //                name: item.name,
-   //                image: item.images.data[0].image_url,
-   //                price: item.best_price,
-   //             });
-   //          });
-   //          setSlide(slideData);
-   //       })
-   //       .catch((err) => {
-   //          console.log(err.message);
-   //       });
-   //    // setBookId('title')
-
-   //    //    === get slides data from api ===
-   // }, []);
-
-
-
-
    return (
       <section>
          <div className="type-two-book-slider-container-outer">
